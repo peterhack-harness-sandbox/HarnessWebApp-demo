679511975abb6c804c0232220c92d6ce5eaa5c38
Peter Hack <peterhack+github@gmail.com>
2021-06-22 09:06:29 -0400

    Update auth-module.php

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,6 +1,6 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
+$auth_version=2; // app Version (1 or 2)
 $auth_icon="";
 $auth_message=""; // Landing page
 $my_link=""; // your link
